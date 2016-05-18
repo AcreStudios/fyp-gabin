a9abfbe81db6ac3257a80d2e85ca7f2a8a7d775b
Aweife <aweifeisaweife@gmail.com>
2016-05-18 17:21:06 +0800

    Added Enemy Debug UI
    Added a new canvas prefab, just drag and drop them onto the enemies and
    call the functions in them to show UI updates!

---
+++ TheGabinProject/Assets/EnemyDebugUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyDebugUI : MonoBehaviour 
+{
+	// Canvas texts
+	Text healthText;
+	Text stateText;
+	Text statusText;
+
+	Transform target;
+
+	
+	void Start() 
+	{
+		healthText = transform.FindChild("Text_Health").GetComponent<Text>();
+		stateText = transform.FindChild("Text_State").GetComponent<Text>();
+		statusText = transform.FindChild("Text_Status").GetComponent<Text>();
+
+		target = GameObject.Find("Player").transform;
+	}
+
+	void Update()
+	{
+		transform.rotation = Quaternion.LookRotation(transform.position - target.position);
+	}
+	
+	public void UpdateHealth(float health)
+	{
+		healthText.text = "Health: " + Mathf.RoundToInt(health);
+	}
+
+	public void UpdateState(string state)
+	{
+		stateText.text = "State: " + state;
+	}
+
+	public void UpdateStatus(string status)
+	{
+		statusText.text = "Status: " + status;
+	}
+}
