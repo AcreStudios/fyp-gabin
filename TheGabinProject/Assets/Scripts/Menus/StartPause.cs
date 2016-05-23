f28117faaf3f318c863f2c389549ef0982cd5f2d
soulagent <goldenwarrior97@gmail.com>
2016-05-24 00:47:24 +0800

    Pause Menu
    Added in a Pause Menu.
    
    Linkage not done. Requesting assistance (lol this sounds so
    professional)

---
+++ TheGabinProject/Assets/Scripts/Menus/StartPause.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPause : MonoBehaviour {
+
+    public Canvas initiatePauseMenu;
+
+	// Use this for initialization
+	void Start () {
+        initiatePauseMenu = initiatePauseMenu.GetComponent<Canvas>();
+
+        initiatePauseMenu.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if((Input.GetKeyDown("P")) && (initiatePauseMenu.enabled = false)) {
+            initiatePauseMenu.enabled = true;
+            Time.timeScale = 0;
+        }
+	}
+}
