88494b30f1f30965c30ffe840fefe1e055fb536b
soulagent <goldenwarrior97@gmail.com>
2016-05-23 19:12:51 +0800

    New Game Flow!
    This will be the first pass for the Game Flow for The Experiment.
    
    Added:
    - Menu Screen with:
    - Play Button
    - Exit Button
    
    - Loading Screen that:
    - Loads World_Test scene after a 5 seconds load time (faked)

---
+++ TheGabinProject/Assets/Scripts/Menus/Loading.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loading : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(TimerThingy());
+    }
+
+    IEnumerator TimerThingy() {
+        yield return new WaitForSeconds(5);
+        Application.LoadLevel("World_Test");
+    }
+}
